7ec30f3b6370adcef451f1f4839a9e30d3880e93
nasre <nasreensheik365@gmIil.com>
2023-10-01 17:43:58 +0530

    code1

---
+++ 06.cart.sh
@@ -0,0 +1,15 @@
+echo -e "\e[32m Downloading Nodejs repo file\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[32m Installing Nodejs server\e[0m"
+yum install nodejs -y
+echo -e "\e[32m Adding user and location\e[0m"
+useradd roboshop
+mkdir /app
+cd /app
+echo -e "\e[32m Downloading new app content and dependencies to cart server\e[0m"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+unzip cart.zip
+rm -rf cart.zip
+npm install
+echo -e "\e[32m creating cart service file\e[0m"
+cp /root/practice-shell/cart.service /etc/systemd/system/cart.service
