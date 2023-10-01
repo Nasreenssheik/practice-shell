748ef854a12bdb09e322a40e065e0d6c851a70bc
nasre <nasreensheik365@gmIil.com>
2023-10-01 18:46:26 +0530

    code1

--- 08.shipping.sh
+++ 08.shipping.sh
@@ -8,3 +8,9 @@ unzip shipping.zip
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 cp /root/practice-shell/shipping.service /etc/systemd/system/shipping.service
+yum install mysql -y
+mysql -h mysql-dev.nasreen.cloud -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[32m Enabling and starting the shipping service\e[0m"
+systemctl daemon-reload
+systemctl enable shipping
+systemctl restart shipping
