f49c98f3b39c3e86264a701dd6026e84e4eede2b
nasre <nasreensheik365@gmIil.com>
2023-10-02 11:09:41 +0530

    code1

--- 03.catalogue.sh
+++ 03.catalogue.sh
@@ -1,23 +1,23 @@
 echo -e "\e[32m Downloading Nodejs repo file\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/catalogue.log
 echo -e "\e[32m Installing Nodejs server\e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/catalogue.log
 echo -e "\e[32m Adding user and location\e[0m"
 useradd roboshop
 mkdir /app
 cd /app
 echo -e "\e[32m Downloading new app content and dependencies to catalogue server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-unzip catalogue.zip
+curl -O https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/catalogue.log
+unzip catalogue.zip &>>/tmp/catalogue.log
 rm -rf catalogue.zip
-npm install
+npm install &>>/tmp/catalogue.log
 echo -e "\e[32m creating catalogue service file\e[0m"
-cp /root/practice-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp /root/practice-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/catalogue.log
 echo -e "\e[32m Downloading and installing the mongodb schema\e[0m"
 cp /root/practice-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb-dev.nasreen.cloud </app/schema/catalogue.js
+yum install mongodb-org-shell -y &>>/tmp/catalogue.log
+mongo --host mongodb-dev.nasreen.cloud </app/schema/catalogue.js &>>/tmp/catalogue.log
 echo -e "\e[32m Enabling and starting the catalogue service\e[0m"
 systemctl daemon-reload
-systemctl enable catalogue
+systemctl enable catalogue &>>/tmp/catalogue.log
 systemctl restart catalogue
