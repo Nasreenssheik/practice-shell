ab02b0f8a0ca25fbd00e7108e49e5d704039578d
nasre <nasreensheik365@gmIil.com>
2023-10-02 11:23:26 +0530

    code1

--- 09.rabbitmq.sh
+++ 09.rabbitmq.sh
@@ -1,11 +1,11 @@
 echo -e "\e[32m Downloading rabbitmq repo file\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/rabbitmq.log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash&>>/tmp/rabbitmq.log
 echo -e "\e[32m Installing Rabbitmq-server\e[0m"
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>/tmp/rabbitmq.log
 echo -e "\e[32m Enabling and starting the Rabbitmq-server\e[0m"
-systemctl enable rabbitmq-server
+systemctl enable rabbitmq-server &>>/tmp/rabbitmq.log
 systemctl start rabbitmq-server
 echo -e "\e[32m Adding user and setting permissions \e[0m"
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/rabbitmq.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/rabbitmq.log
