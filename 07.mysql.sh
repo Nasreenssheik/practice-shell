849fe60fc84bb2470f5726d8954e5ddaf1ee75e5
nasre <nasreensheik365@gmIil.com>
2023-10-02 10:40:46 +0530

    code1

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -4,11 +4,10 @@ echo -e "\e[32m Setuping the MySQL5.7 repo file\e[0m"
 cp /root/practice-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[32m Installing mysql server\e[0m"
 yum install mysql-community-server -y
-echo -e "\e[32m Enabling and starting Mysql server\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
 echo -e "\e[32m Changing default root password\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 echo -e "\e[32m Checking new password properly working or not\e[0m"
 mysql -uroot -pRoboShop@1
-echo -e "\e[32m Mysql server setup successfully completed\e[0m"
+echo -e "\e[32m Enabling and starting Mysql server\e[0m"
+systemctl enable mysqld
+systemctl restart mysqld
