4661693f10c0312c5c963ecf73b20037a1219dbd
nasre <nasreensheik365@gmIil.com>
2023-10-02 10:52:46 +0530

    code1

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -1,6 +1,6 @@
 echo -e "\e[32m Disabiling Mysql default version\e[0m"
 yum module disable mysql -y
-echo -e "\e[32m Setuping the MySQL5.7 repo file\e[0m"
+echo -e "\e[32m Setting up the MySQL5.7 repo file\e[0m"
 cp /root/practice-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[32m Installing mysql server\e[0m"
 yum install mysql-community-server -y
