8ebc3370fbbfa8196989f200a66c7bf6b5f5068e
nasre <nasreensheik365@gmIil.com>
2023-10-02 11:04:16 +0530

    code1

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,14 +1,14 @@
 echo -e "\e[33m Installing Nginx Server\e[0m"
-yum install nginx -y
+yum install nginx -y &>>/tmp/frontend.log
 echo -e "\e[33m Removing default content\e[0m"
 cd /usr/share/nginx/html
-rm -rf *
+rm -rf * &>>/tmp/frontend.log
 echo -e "\e[33m Downloading new content to nginx server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-unzip frontend.zip
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/frontend.log
+unzip frontend.zip &>>/tmp/frontend.log
 rm -rf frontend.zip
 echo -e "\e[33m Configuring reverse proxy nginx server\e[0m"
 cp /root/practice-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[33m enabling and starting nginx server\e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>/tmp/frontend.log
 systemctl restart nginx
