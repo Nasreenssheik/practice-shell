e028b0429edb8cfbc2084cae159a6b0a174d253f
nasre <nasreensheik365@gmIil.com>
2023-10-02 11:27:51 +0530

    code1

--- 11.dispatch.sh
+++ 11.dispatch.sh
@@ -1,18 +1,18 @@
 echo -e "\e[32m Installing golang server\e[0m"
-yum install golang -y
+yum install golang -y &>>/tmp/dispatch.log
 echo -e "\e[32m Adding user and location\e[0m"
-useradd roboshop
-mkdir /app
+useradd roboshop &>>/tmp/dispatch.log
+mkdir /app &>>/tmp/dispatch.log
 cd /app
 echo -e "\e[32m Downloading new app content,dependencies and building software to dispatch server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-unzip dispatch.zip
-go mod init dispatch
-go get
-go build
+curl -O https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>/tmp/dispatch.log
+unzip dispatch.zip &>>/tmp/dispatch.log
+go mod init dispatch &>>/tmp/dispatch.log
+go get &>>/tmp/dispatch.log
+go build &>>/tmp/dispatch.log
 echo -e "\e[32m creating dispatch service file\e[0m"
 cp /root/practice-shell/dispatch.service /etc/systemd/system/dispatch.service
 echo -e "\e[32m Enabling and starting the dispatch service\e[0m"
 systemctl daemon-reload
-systemctl enable dispatch
+systemctl enable dispatch &>>/tmp/dispatch.log
 systemctl restart dispatch
