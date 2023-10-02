6a6202af8e7aee79ad848e10d79d4cd402224752
nasre <nasreensheik365@gmIil.com>
2023-10-02 11:06:31 +0530

    code1

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -1,10 +1,10 @@
 echo -e "\e[32m Downloading Mongodb Repo\e[0m"
-cp /root/practice-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp /root/practice-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/mongodb.log
 echo -e "\e[32m Installing Mongodb server\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/mongodb.log
 echo -e "\e[32m Changing the listen address\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/mongodb.log
 echo -e "\e[32m Enabling and starting Mongodb server\e[0m"
-systemctl enable mongod
+systemctl enable mongod &>>/tmp/mongodb.log
 systemctl restart mongod
 
