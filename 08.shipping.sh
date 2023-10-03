885edbcb1fbf152cbc8c80b17e4027417c218277
nasre <nasreensheik365@gmIil.com>
2023-10-03 14:00:47 +0530

    code1

--- 08.shipping.sh
+++ 08.shipping.sh
@@ -1,21 +1,26 @@
-echo -e "\e[32m Installing maven server\e[0m"
-yum install maven -y &>>/tmp/shipping.log
-echo -e "\e[32m Adding user and location\e[0m"
-useradd roboshop &>>/tmp/shipping.log
-mkdir /app &>>/tmp/shipping.log
-cd /app
-echo -e "\e[32m Downloading new app content to shipping server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/shipping.log
-unzip shipping.zip &>>/tmp/shipping.log
-echo -e "\e[32m Downloading dependencies and builiding application to shipping server\e[0m"
-mvn clean package &>>/tmp/shipping.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/shipping.log
-echo -e "\e[32m creating shipping service file\e[0m"
+color="\e[32m"
+noclor="\e[0m"
+logfile="/tmp/roboshop.log"
+app_path="/app"
+
+echo -e "$color Installing maven server$nocolor"
+yum install maven -y &>>${logfile}
+echo -e "$color Adding user and location$nocolor"
+useradd roboshop &>>${logfile}
+mkdir ${app_path} &>>${logfile}
+cd ${app_path}
+echo -e "$color Downloading new app content to shipping server$nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>${logfile}
+unzip shipping.zip &>>${logfile}
+echo -e "$color Downloading dependencies and builiding application to shipping server$nocolor"
+mvn clean package &>>${logfile}
+mv target/shipping-1.0.jar shipping.jar &>>${logfile}
+echo -e "$color creating shipping service file$nocolor"
 cp /root/practice-shell/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[32m Downloading and installing the mysql schema\e[0m"
-yum install mysql -y &>>/tmp/shipping.log
-mysql -h mysql-dev.nasreen.cloud -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/shipping.log
-echo -e "\e[32m Enabling and starting the shipping service\e[0m"
+echo -e "$color Downloading and installing the mysql schema$nocolor"
+yum install mysql -y &>>${logfile}
+mysql -h mysql-dev.nasreen.cloud -uroot -pRoboShop@1 < ${app_path}/schema/shipping.sql &>>${logfile}
+echo -e "$color Enabling and starting the shipping service$nocolor"
 systemctl daemon-reload
-systemctl enable shipping &>>/tmp/shipping.log
+systemctl enable shipping &>>${logfile}
 systemctl restart shipping
