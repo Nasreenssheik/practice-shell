5c4fb545e473b374639331b7a83eccebee3c7930
nasre <nasreensheik365@gmIil.com>
2023-10-03 12:07:27 +0530

    code1

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,18 +1,18 @@
 color="\e[33m"
 noclor="\e[0m"
-log_file="&>>/tmp/frontend.log"
+log_file="/tmp/frontend.log"
 
 echo -e "$color Installing Nginx Server$nocolor"
-yum install nginx -y $log_file
+yum install nginx -y &>>${logfile}
 echo -e "$color Removing default content$nocolor"
 cd /usr/share/nginx/html
-rm -rf * $log_file
+rm -rf * &>>${logfile}
 echo -e "$color Downloading new content to nginx server$nocolor"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip $log_file
-unzip frontend.zip $log_file
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${logfile}
+unzip frontend.zip &>>${logfile}
 rm -rf frontend.zip
 echo -e "$color Configuring reverse proxy nginx server$nocolor"
 cp /root/practice-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "$color enabling and starting nginx server$nocolor"
-systemctl enable nginx $log_file
+systemctl enable nginx &>>${logfile}
 systemctl restart nginx
