ae2c4ec8bbcc48e1af8199be6fb3c3987a541d16
nasre <nasreensheik365@gmIil.com>
2023-10-03 13:45:35 +0530

    code1

--- 05.user.sh
+++ 05.user.sh
@@ -1,23 +1,27 @@
-echo -e "\e[32m Downloading Nodejs repo file\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/user.log
-echo -e "\e[32m Installing Nodejs server\e[0m"
-yum install nodejs -y &>>/tmp/user.log
-echo -e "\e[32m Adding user and location\e[0m"
-useradd roboshop  &>>/tmp/user.log
-mkdir /app  &>>/tmp/user.log
+color="\e[32m"
+noclor="\e[0m"
+logfile="/tmp/roboshop.log"
+
+echo -e "$color Downloading Nodejs repo file$nocolor"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${logfile}
+echo -e "$color Installing Nodejs server$nocolor"
+yum install nodejs -y &>>${logfile}
+echo -e "$color Adding user and location$nocolor"
+useradd roboshop  &>>${logfile}
+mkdir /app &>>${logfile}
 cd /app
-echo -e "\e[32m Downloading new app content and dependencies to user server\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/user.zip  &>>/tmp/user.log
-unzip user.zip  &>>/tmp/user.log
+echo -e "$color Downloading new app content and dependencies to user server$nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/user.zip  &>>${logfile}
+unzip user.zip &>>${logfile}
 rm -rf user.zip
-npm install  &>>/tmp/user.log
-echo -e "\e[32m creating user service file\e[0m"
+npm install  &>>${logfile}
+echo -e "$color creating user service file$nocolor"
 cp /root/practice-shell/user.service /etc/systemd/system/user.service
-echo -e "\e[32m Downloading and installing the mongodb schema\e[0m"
+echo -e "$color Downloading and installing the mongodb schema$nocolor"
 cp /root/practice-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y  &>>/tmp/user.log
-mongo --host mongodb-dev.nasreen.cloud </app/schema/user.js  &>>/tmp/user.log
-echo -e "\e[32m Enabling and starting the user service\e[0m"
+yum install mongodb-org-shell -y  &>>${logfile}
+mongo --host mongodb-dev.nasreen.cloud </app/schema/user.js  &>>${logfile}
+echo -e "$color Enabling and starting the user service$nocolor"
 systemctl daemon-reload
-systemctl enable user &>>/tmp/user.log
+systemctl enable user &>>${logfile}
 systemctl restart
