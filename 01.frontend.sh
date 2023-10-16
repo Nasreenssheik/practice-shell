076a64a5e5b4cb438b813f4a6ab0ea2d9444dc9a
nasre <nasreensheik365@gmIil.com>
2023-10-16 11:12:16 +0530

    code1

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -12,11 +12,11 @@ echo -e "$color Downloading new content to ${component} server$nocolor"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${logfile}
 unzip frontend.zip &>>${logfile}
 rm -rf frontend.zip
-status_check
+status_check $?
 echo -e "$color Configuring reverse proxy ${component} server$nocolor"
 cp /root/practice-shell/roboshop.conf /etc/${component}/default.d/roboshop.conf
-status_check
+status_check $?
 echo -e "$color enabling and starting ${component} server$nocolor"
 systemctl enable ${component} &>>${logfile}
 systemctl restart ${component}
-status_check
+status_check $?
